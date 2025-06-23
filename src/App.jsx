af7dea200de89562935fefe5a74503e34109e52b
Kanika <kanisd27@gmail.com>
2025-06-23 11:14:09 +0530

    day7

--- src/App.jsx
+++ src/App.jsx
@@ -119,7 +119,7 @@ function App(){
 }
 export default App */
 
-import React, { useState } from 'react';
+/*import React, { useState } from 'react';
 
 function App() {
   const [userList, setUserList] = useState([]);
@@ -186,4 +186,30 @@ function App() {
   );
 }
 
+export default App; */
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Name from './Name';
+import About from './About';
+import Placement from './Placement';
+
+function App() {
+  return (
+    <Router>
+      <nav style={{ display: 'flex', gap: '1rem', padding: '1rem', background: '#eee' }}>
+        <Link to="/">Name</Link>
+        <Link to="/about">About</Link>
+        <Link to="/placement">Placement</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Name />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/placement" element={<Placement />} />
+      </Routes>
+    </Router>
+  );
+}
+
 export default App;
+
